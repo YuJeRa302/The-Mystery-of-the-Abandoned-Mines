4b62468eca5a7da3623e8ed8aa8a363e549620d7
YuJeRa302 <71927873+YuJeRa302@users.noreply.github.com>
2025-02-03 22:35:43 +0300

    Rewrite AbilityAttributeData

--- Assets/Source/Game/Scripts/ScriptableObject/Abillity/AbilityAttributeData.cs
+++ Assets/Source/Game/Scripts/ScriptableObject/Abillity/AbilityAttributeData.cs
@@ -2,16 +2,20 @@ using UnityEngine;
 
 namespace Assets.Source.Game.Scripts
 {
-    [CreateAssetMenu(fileName = "New Ability Attribute", menuName = "Create Ability Attribute", order = 51)]
+    [CreateAssetMenu(fileName = "New Ability Attribute", menuName = "Create Attribute/Ability Attribute", order = 51)]
     public abstract class AbilityAttributeData : AttributeData
     {
         [SerializeField] private TypeAbility _abilityType;
         [SerializeField] private AbilityView _abilityView;
         [SerializeField] private ParticleSystem _particleSystem;
         [SerializeField] private AudioClip _audioClip;
+        [SerializeField] private TypeMagic _typeMagic;
+        [SerializeField] private LegendaryAbilityData _legendaryAbilityData;
 
+        public LegendaryAbilityData LegendaryAbilityData => _legendaryAbilityData;
         public AbilityView AbilityView => _abilityView;
-        public TypeAbility AbilityType => _abilityType;
+        public TypeAbility TypeAbility => _abilityType;
+        public TypeMagic TypeMagic => _typeMagic;
         public ParticleSystem ParticleSystem => _particleSystem;
         public AudioClip AudioClip => _audioClip;
     }
